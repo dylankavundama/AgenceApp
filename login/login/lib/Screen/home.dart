0e20ac9353d1dd4b05c6d0af6621acf56267cbb7
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2023-12-31 13:24:49 +0100

    go

---
+++ login/login/lib/Screen/home.dart
@@ -0,0 +1,17 @@
+
+import 'package:lezzart/Qr.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _user = Provider.of<User?>(context);
+    return Scaffold(
+      body: QRCodeScannerApp()
+    );
+  }
+}
